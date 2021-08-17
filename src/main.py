8ea6e6026c82d95f48554bc8ca045a4691d5cc77
imohammad12 <m25dehgh@uwaterloo.ca>
2021-08-17 19:34:21 -0400

    main.py is updated:  - loop for del_threshold is deleted; set static to 1.1  - par_threshold is set between .7 and .8

--- src/main.py
+++ src/main.py
@@ -57,28 +57,25 @@ elif config['operation'] == "sample":
 	from tree_edits_beam import *
 
 	# Testing multiple configurations
-	for del_threshold in np.arange(0.85, 1.25, 0.05):
-		for par_threshold in np.arange(0.85, 1.25, 0.05):
+	# for del_threshold in np.arange(0.85, 1.25, 0.05):
+	for par_threshold in np.arange(0.70, 0.85, 0.05):
 
-			if del_threshold < 0.9 and par_threshold < 1.1:
-				continue
+		config = load_config()
 
-			config = load_config()
+		config['threshold']['par'] = par_threshold
+		# config['threshold']['dl'] = del_threshold
 
-			config['threshold']['par'] = par_threshold
-			config['threshold']['dl'] = del_threshold
+		save_config(config)
 
-			save_config(config)
+		importlib.reload(sys.modules['utils'])
+		from utils import *
 
-			importlib.reload(sys.modules['utils'])
-			from utils import *
+		if config['set'] == 'valid':
+			sample(valid_complex, valid_simple, output_lang, tag_lang, dep_lang, lm_forward, lm_backward, output_embedding_weights, idf, unigram_prob, start_time, load_config())
+		elif config['set'] == 'test':
+			sample(test_complex, test_simple, output_lang, tag_lang, dep_lang, lm_forward, lm_backward, output_embedding_weights, idf, unigram_prob, start_time, load_config())
 
-			if config['set'] == 'valid':
-				sample(valid_complex, valid_simple, output_lang, tag_lang, dep_lang, lm_forward, lm_backward, output_embedding_weights, idf, unigram_prob, start_time, load_config())
-			elif config['set'] == 'test':
-				sample(test_complex, test_simple, output_lang, tag_lang, dep_lang, lm_forward, lm_backward, output_embedding_weights, idf, unigram_prob, start_time, load_config())
-
-			open(config['file_name'], "w").close()  # changed
+		open(config['file_name'], "w").close()  # changed
 
 	end = time.time()
 	print(f"Runtime of the program is {end - start_time}")
