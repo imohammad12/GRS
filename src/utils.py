66e669cd54e2cedc2add861d80ad2c76e17648a3
imohammad12 <m25dehgh@uwaterloo.ca>
2021-08-24 13:45:50 -0400

    data path in utils.py updated

--- src/utils.py
+++ src/utils.py
@@ -186,10 +186,9 @@ class Lang:
             #                  encoding='utf-8').read().split('\n')
             # train_dst = open('/home/m25dehgh/simplification/asset/dataset/asset.test.simp.0',
             #                  encoding='utf-8').read().split('\n')
-            valid_src = open('/home/m25dehgh/simplification/datasets/asset/dataset/asset.valid.orig',
-                             encoding='utf-8').read().split('\n')
-            valid_dst = open('/home/m25dehgh/simplification/datasets/asset/dataset/asset.valid.simp.0',
-                             encoding='utf-8').read().split('\n')
+            valid_src = open(config['orig_file_path'], encoding='utf-8').read().split('\n')
+            valid_dst = open(config['ref_folder_path'] + "/" + 'test.8turkers.tok.turk.0',
+                            encoding='utf-8').read().split('\n')
             test_src = open(config['orig_file_path'], encoding='utf-8').read().split('\n')
             # test_dst = open(config['ref_folder_path'] + "/" + 'V0V4_V1V4_V2V4_V3V4_V0V3_V0V2_V1V3.aner.ori.test.dst',
             #                 encoding='utf-8').read().split('\n')
