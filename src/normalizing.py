3301fbc6806bb4ef00898bdb5a11a27d4ba4dccb
imohammad12 <m25dehgh@uwaterloo.ca>
2021-09-06 13:06:53 -0400

    disabling normalizing for str inputs

--- src/normalizing.py
+++ src/normalizing.py
@@ -28,8 +28,9 @@ def normalize(sentence, lowercase: bool = True, tokenizer: str = '13a', return_s
 
 def all_norms(sentences: Union[str, List[str]]):
     if type(sentences) == str:
-        for x in ['13a', 'intl', 'moses', 'penn']:
-            sentences = normalize(sentences, tokenizer=x)
+        pass
+        # for x in ['13a', 'intl', 'moses', 'penn']:
+            # sentences = normalize(sentences, tokenizer=x)
 
     else:
         output = []
