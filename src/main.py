be4790d2e30bef5629c8026d773560518873e622
imohammad12 <m25dehgh@uwaterloo.ca>
2022-01-29 17:23:25 +0330

    bug fixed in main

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import numpy as np
 from transformers import DebertaForSequenceClassification, Trainer, TrainingArguments, DebertaTokenizerFast
 from ccd import ComplexComponentDetector
 from model.structural_decoder import DecoderGRU
+from tree_edits_beam import *
 
 # importlib.reload(sys.modules['utils'])
 # importlib.reload(sys.modules['config'])
@@ -27,12 +28,6 @@ model_name_grammar_checker = "deberta-base-cola"
 path = root_grammar_checker + '/results' + '/' + model_name_grammar_checker + "/checkpoint-716"
 model_grammar_checker = DebertaForSequenceClassification.from_pretrained(path)
 
-print('Creating ccd object...')
-ccd = ComplexComponentDetector.combined_version(idf,
-                                                output_lang,
-                                                comp_simp_class_model=comp_simp_class_model,
-                                                tokenizer=tokenizer_deberta,
-                                                **config)
 
 asset_paths = {
     "log_directory": "/home/m25dehgh/simplification/outputs/asset/whole-dataset",
@@ -75,17 +70,24 @@ lm_backward = DecoderGRU(config['hidden_size'], output_lang.n_words, tag_lang.n_
                          config['embedding_dim'], config['tag_dim'], config['dep_dim'], config['dropout'],
                          config['use_structural_as_standard']).to(device)
 
+print('Creating ccd object...')
+ccd = ComplexComponentDetector.combined_version(idf,
+                                                output_lang,
+                                                comp_simp_class_model=comp_simp_class_model,
+                                                tokenizer=tokenizer_deberta,
+                                                **config)
+
 open(config['file_name'], "w").close()
 
 start_time = time.time()
 
-from tree_edits_beam import *
+
 
 # Testing multiple configurations
 # for i, del_threshold in enumerate(np.arange(1.1, 1.5, 0.1)):
 # for j, par_thresh in enumerate(np.arange(0.6, 1.1, 0.1)):
 
-config = load_config()
+# config = load_config()
 
 # 	config['threshold']['par'] = 0.8
 # 	config['threshold']['dl'] = 2.0
@@ -98,10 +100,10 @@ config = load_config()
 # config['delete_leaves'] = False
 
 # config['threshold']['par'] = np.round(par_thresh, 2)
-config['threshold']['par'] = 1.0
-config['threshold']['dl'] = np.round(del_threshold, 2)
+# config['threshold']['par'] = 1.0
+# config['threshold']['dl'] = np.round(del_threshold, 2)
 
-save_config(config)
+# save_config(config)
 
 # importlib.reload(sys.modules['utils'])
 # from utils import *
