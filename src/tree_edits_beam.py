d8713626701d28cfe3d18a155637de4630c7f34a
imohammad12 <m25dehgh@uwaterloo.ca>
2022-02-09 18:00:29 +0330

    tree_edits_beam.py updated for ablation study of operations on asset

--- src/tree_edits_beam.py
+++ src/tree_edits_beam.py
@@ -102,7 +102,7 @@ def sample(complex_sentences, simple_sentences, input_lang, tag_lang, dep_lang, 
 
     runtime = time.time() - start_time
     all_scores = {**sari_scores, **simil_simp_gram_scores, **stats, "runtime": runtime}
-    # ccd.params.update(config)
+    ccd.params.update(config)
     config.update(ccd.params)
     print("all scores", all_scores)
 
